fdce51afefebf56756ee643980e88bcd489d08fc
Daniel Silva <danielcnsrj@gmail.com>
2018-02-24 19:55:53 -0300

    Criação do componente item-cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ItemCadastroComponent
   ],
   imports: [
     BrowserModule
