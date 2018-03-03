9b9a43a28952ea1811df60a11954b891e984c01b
Daniel Silva <danielcnsrj@gmail.com>
2018-03-02 23:02:40 -0300

    Implementação da chamada ao backend

--- src/app/item-cadastro/item-cadastro.component.ts
+++ src/app/item-cadastro/item-cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ItemService } from "../item.service";
+
 @Component({
   selector: 'app-item-cadastro',
   templateUrl: './item-cadastro.component.html',
@@ -7,14 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemCadastroComponent implements OnInit {
 
-  itens = [
-    { etiqueta: 'AA1234', descricao: 'Notebook', dataAquisicao: new Date() },
-    { etiqueta: 'BB9876', descricao: 'Mouse', dataAquisicao: new Date() }
-  ];
+  itens = [];
 
-  constructor() { }
+  constructor( private itemService: ItemService) { }
 
   ngOnInit() {
+    this.consultar();
   }
 
+  consultar(){
+    this.itemService.listar().subscribe( dados => this.itens = dados );
+  }
 }
